5d74ff2c36f6997f7d8ef10ffa49f55dfd5340d6
xianlu <xianlu.cxl@alibaba-inc.com>
2019-03-11 16:26:44 +0800

    Add retry and three times check support

--- workflow/controller/wrapper.go
+++ workflow/controller/wrapper.go
@@ -2,12 +2,19 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	wfclientset "github.com/argoproj/argo/pkg/client/clientset/versioned"
+	"github.com/argoproj/argo/workflow/util"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"strconv"
 	"time"
 )
+
+var workflowRecordLimitMap = make(map[string]int)
+const aliyunRetryKey = "aliyun.retry"
 
 // ReSyncWorkflow lookup all workflow on incorrect status
 func (wfc *WorkflowController) ReSyncWorkflow(ctx context.Context) {
@@ -32,7 +39,7 @@ func (wfc *WorkflowController) ReSyncWorkflow(ctx context.Context) {
 				continue
 			}
 
-			checkDagWorkflowStatus(&wf, wfc.wfclientset)
+			checkDagWorkflowStatus(&wf, wfc.wfclientset, wfc.kubeclientset)
 		}
 
 		time.Sleep(5 * time.Minute)
@@ -40,7 +47,7 @@ func (wfc *WorkflowController) ReSyncWorkflow(ctx context.Context) {
 
 }
 
-func checkDagWorkflowStatus(wf *v1alpha1.Workflow, argoClient wfclientset.Interface) {
+func checkDagWorkflowStatus(wf *v1alpha1.Workflow, argoClient wfclientset.Interface, kubeClient kubernetes.Interface) {
 	if wf == nil {
 		return
 	}
@@ -74,12 +81,57 @@ func checkDagWorkflowStatus(wf *v1alpha1.Workflow, argoClient wfclientset.Interf
 		return
 	}
 
+	mapKey := fmt.Sprintf("%s-%s", wf.Namespace, wf.Name)
+	if val, ok := workflowRecordLimitMap[mapKey]; ok {
+		if val < 3 {
+			workflowRecordLimitMap[mapKey] += 1
+			return
+		}
+	} else {
+		workflowRecordLimitMap[mapKey] = 1
+		return
+	}
+
+	retryArgoInterface := argoClient.ArgoprojV1alpha1().Workflows(wf.Namespace)
 	log.Infof("ReSync Workflow %s:%s status from %s to %s", wf.Namespace, wf.Name, wf.Status.Phase, v1alpha1.NodeFailed)
 	dagNode.Phase = v1alpha1.NodeFailed
 	wf.Status.Nodes[targetNode[0].ID] = dagNode
 	wf.Status.Phase = v1alpha1.NodeFailed
+
+	wfLabelsMap := wf.Labels
+	wfRetryTimesInt := 0
+	if retryTimesStr, ok := wfLabelsMap[aliyunRetryKey]; !ok {
+		wfLabelsMap[aliyunRetryKey] = "1"
+	} else {
+		tmpRetryInt, tmpErr := strconv.Atoi(retryTimesStr)
+		if tmpErr != nil {
+			log.Error(tmpErr)
+			tmpRetryInt = 0
+		}
+		tmpRetryInt++
+		wfLabelsMap[aliyunRetryKey] = strconv.Itoa(tmpRetryInt)
+		wfRetryTimesInt = tmpRetryInt
+	}
+	wf.SetLabels(wfLabelsMap)
+
 	_, err := argoClient.ArgoprojV1alpha1().Workflows(wf.Namespace).Update(wf)
 	if err != nil {
 		log.Errorf("Update workflow %s:%s error %v", wf.Namespace, wf.Name, err)
 	}
+
+	time.Sleep(10 * time.Second)
+	if wfRetryTimesInt > 3 {
+		return
+	}
+	wf, err = retryArgoInterface.Get(wf.Name, v1.GetOptions{})
+	if err != nil {
+		log.Errorf("Get workflow %s:%s error %v",  wf.Namespace, wf.Name, err)
+		return
+	}
+	log.Infof("Retry workflow %s:%s for unstable status map value %d", wf.Namespace, wf.Name, workflowRecordLimitMap[mapKey])
+	if _, tmpErr := util.RetryWorkflow(kubeClient, retryArgoInterface, wf); tmpErr != nil {
+		log.Errorf("Retry workflow %s error %v", wf.Name, tmpErr)
+	}
+
+	delete(workflowRecordLimitMap, mapKey)
 }
