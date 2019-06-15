bd49391e8ee58b07fc959bd284ced49802d92291
xianlu <xianlu.cxl@alibaba-inc.com>
2019-06-15 14:02:28 +0800

    Remove resin workflow

--- cmd/workflow-controller/main.go
+++ cmd/workflow-controller/main.go
@@ -77,7 +77,6 @@ func NewRootCommand() *cobra.Command {
 			go wfController.MetricsServer(ctx)
 			go wfController.TelemetryServer(ctx)
 			go wfController.RunTTLController(ctx)
-			go wfController.ReSyncWorkflow(ctx)
 
 			// Wait forever
 			select {}
